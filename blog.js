0b2caba11d654e55872826f132dc082728bb88e9
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-09-14 00:14:50 +0600

    fifth-commit

--- blog.js
+++ blog.js
@@ -1,5 +1,6 @@
 const loadBlogData =()=> {
     const blogDiv=document.getElementById('blog-2');
+    blogDiv.style.border="1px solid blue";
     const li=document.createElement('li');
     li.innerText='';
     li.innerText=`var and let are both used for variable declaration in javascript but the difference between them is that var is function scoped and let is block scoped.
